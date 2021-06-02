04c249404473027f8139315635a9a7d3086399bc
Andrew <35283726+switch900@users.noreply.github.com>
2021-06-01 19:39:40 -0700

    Add project files.

---
+++ HVACProject3/Controllers/HVACEquipmentsController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HVACProject3.Models;
+
+namespace HVACProject3.Controllers
+{
+    [Route("api/HVACEquipments")]
+    [ApiController]
+    public class HVACEquipmentsController : ControllerBase
+    {
+        private readonly HVACEquipmentContext _context;
+
+        public HVACEquipmentsController(HVACEquipmentContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/HVACEquipments
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<HVACEquipment>>> GetHVACEquipments()
+        {
+            return await _context.HVACEquipments.ToListAsync();
+        }
+
+        // GET: api/HVACEquipments/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<HVACEquipment>> GetHVACEquipment(long id)
+        {
+            var hVACEquipment = await _context.HVACEquipments.FindAsync(id);
+
+            if (hVACEquipment == null)
+            {
+                return NotFound();
+            }
+
+            return hVACEquipment;
+        }
+
+        // PUT: api/HVACEquipments/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutHVACEquipment(long id, HVACEquipment hVACEquipment)
+        {
+            if (id != hVACEquipment.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(hVACEquipment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HVACEquipmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/HVACEquipments
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<HVACEquipment>> PostHVACEquipment(HVACEquipment hVACEquipment)
+        {
+            _context.HVACEquipments.Add(hVACEquipment);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetHVACEquipment), new { id = hVACEquipment.Id }, hVACEquipment);
+        }
+
+
+        // DELETE: api/HVACEquipments/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteHVACEquipment(long id)
+        {
+            var hVACEquipment = await _context.HVACEquipments.FindAsync(id);
+            if (hVACEquipment == null)
+            {
+                return NotFound();
+            }
+
+            _context.HVACEquipments.Remove(hVACEquipment);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool HVACEquipmentExists(long id)
+        {
+            return _context.HVACEquipments.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ HVACProject3/Models/HVACEquipmentContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HVACProject3.Models
+{
+    public class HVACEquipmentContext : DbContext
+    {
+
+        public HVACEquipmentContext(DbContextOptions<HVACEquipmentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HVACEquipment> HVACEquipments { get; set; }
+    }
+}
+
