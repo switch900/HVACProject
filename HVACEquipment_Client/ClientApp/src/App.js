d77bcb06bc341e5d14031b28bdea01d0fc64a3a9
andrew Hewitson <switch_900@hotmail.com>
2021-06-10 13:54:07 -0700

    Added new client to use redux

--- HVACEquipment_Client/ClientApp/src/App.js
+++ HVACEquipment_Client/ClientApp/src/App.js
@@ -4,19 +4,37 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import NavBar from "./components/NavBar";
 
-import './custom.css'
 
-export default class App extends Component {
-  static displayName = App.name;
+//import './custom.css'
 
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
+////export default class App extends Component {
+////  static displayName = App.name;
+
+////  render () {
+////    return (
+////      <Layout>
+////        <Route exact path='/' component={Home} />
+////        <Route path='/counter' component={Counter} />
+////        <Route path='/fetch-data' component={FetchData} />
+////      </Layout>
+////    );
+////  }
+////}
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <div class="Header">
+                    <h1>Andrew Hewitson</h1>
+                    <h3>React Portfolio</h3>
+                </div>
+                <NavBar />
+            </div>
+        );
+    }
 }
+
+export default App;
---
+++ HVACEquipment_Client/ClientApp/src/components/NotFound.js
@@ -0,0 +1,9 @@
+﻿function NotFound() {
+    return (
+        <div style={{ backgroundColor: "blue", height: "500px" }}>
+            <h2>NotFound</h2>
+        </div>
+    );
+}
+
+export default NotFound;
