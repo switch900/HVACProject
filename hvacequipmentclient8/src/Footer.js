57beab83bfc802bc87fb63396bf2cd87a3f3d534
andrew Hewitson <switch_900@hotmail.com>
2021-07-14 15:27:24 -0700

    add social media icons to footer

--- hvacequipmentclient8/src/Footer.js
+++ hvacequipmentclient8/src/Footer.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { SocialIcon } from 'react-social-icons';
 import './App.css';
+import Phone from './images/phone.png';
+
 
 // import {
 //     NavItem,
@@ -29,7 +32,7 @@ export class Footer extends Component {
             <div className="footerContainer">
                 <footer className="navbar navbar-expand-sm bg-dark navbar-dark justify-content-center fixed-bottom">
                     <div className="footerStyle">
-                        <h3>Andrew Hewitson 2021</h3>
+
 
                         <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
                             <ul className="navbar-nav ml-auto">
@@ -42,11 +45,22 @@ export class Footer extends Component {
                                 <li>
                                     <Link className="nav-link" to="/contact">Contact Me</Link>
                                 </li>
+                                <li>
+                                    <Link className="nav-link" to="/contact">Contact Me</Link>
+                                </li>
                             </ul>
+                        </div>
+                        <div className="footDivide" style={{ textalign: "center" }} >__________________________________________________________________</div>
+                        {/* <h3>Andrew Hewitson 2021</h3> */}
+                        <div class="grid-container">
+                            <div class="grid-item"><SocialIcon url="mailto:webmaster@example.com" style={{ height: 25, width: 25 }} /> </div>
+                            <div class="grid-item"> <SocialIcon url="https://github.com/switch900" style={{ height: 25, width: 25 }} /></div>
+                            <div class="grid-item"> <SocialIcon url="https://www.linkedin.com/in/andrew-hewitson/" style={{ height: 25, width: 25 }} /></div>
+                            <div class="grid-item"> <a href="tel:778-228-2070"><img src={Phone} alt="Phone" style={{ height: 25, width: 25 }} /></a></div>
                         </div>
                     </div>
                 </footer>
-            </div>
+            </div >
         );
     }
 }
