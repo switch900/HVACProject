100ca6864157603cd31dc516f90f6f17190974e5
andrew Hewitson <switch_900@hotmail.com>
2021-07-14 15:37:18 -0700

    mawr footer tweaks

--- hvacequipmentclient8/src/Footer.js
+++ hvacequipmentclient8/src/Footer.js
@@ -32,31 +32,14 @@ export class Footer extends Component {
             <div className="footerContainer">
                 <footer className="navbar navbar-expand-sm bg-dark navbar-dark justify-content-center fixed-bottom">
                     <div className="footerStyle">
-
-
-                        <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-                            <ul className="navbar-nav ml-auto">
-                                <li>
-                                    <Link className="nav-link" to="/about">About This Project</Link>
-                                </li>
-                                <li>
-                                    <Link className="nav-link" to="/about">Other Cool Things</Link>
-                                </li>
-                                <li>
-                                    <Link className="nav-link" to="/contact">Contact Me</Link>
-                                </li>
-                                <li>
-                                    <Link className="nav-link" to="/contact">Contact Me</Link>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className="footDivide" style={{ textalign: "center" }} >__________________________________________________________________</div>
-                        {/* <h3>Andrew Hewitson 2021</h3> */}
+                        <h3>Andrew Hewitson</h3>
                         <div class="grid-container">
                             <div class="grid-item"><SocialIcon url="mailto:webmaster@example.com" style={{ height: 25, width: 25 }} /> </div>
                             <div class="grid-item"> <SocialIcon url="https://github.com/switch900" style={{ height: 25, width: 25 }} /></div>
                             <div class="grid-item"> <SocialIcon url="https://www.linkedin.com/in/andrew-hewitson/" style={{ height: 25, width: 25 }} /></div>
                             <div class="grid-item"> <a href="tel:778-228-2070"><img src={Phone} alt="Phone" style={{ height: 25, width: 25 }} /></a></div>
+                            <div class="grid-item"> <SocialIcon url="/about" style={{ height: 25, width: 25 }} /></div>
+                            <div class="grid-item"> <SocialIcon url="/contact" style={{ height: 25, width: 25 }} /></div>
                         </div>
                     </div>
                 </footer>
